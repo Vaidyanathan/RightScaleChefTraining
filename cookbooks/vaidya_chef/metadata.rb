4292c124c867305f1de3c05dd115d956fa3b8738
Vaidyanathan <vvaidyanathan@ctepl.com>
2012-02-27 18:08:49 +0000

    Your commit message

--- cookbooks/vaidya_chef/metadata.rb
+++ cookbooks/vaidya_chef/metadata.rb
@@ -1,8 +1,8 @@
 maintainer       "Vaidya"
-maintainer_email "vvaidyanathan@ctepl.com""
+maintainer_email "vvaidyanathan@ctepl.com"
 license          "All rights reserved"
 description      "Installs/Configures vaidya_chef"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 
-recipe "rjg_chef::default", "Prints a message to the output"
+recipe "vaidya_chef::default", "Prints a message to the output"
