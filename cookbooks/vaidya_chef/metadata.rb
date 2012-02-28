ca94998de5febfc0424ae3ec2d263dfda30585af
Vaidyanathan <vvaidyanathan@ctepl.com>
2012-02-28 16:15:27 +0000

    Your message here

--- cookbooks/vaidya_chef/metadata.rb
+++ cookbooks/vaidya_chef/metadata.rb
@@ -8,3 +8,9 @@ version          "0.0.1"
 depends "rs_utils"
 
 recipe "vaidya_chef::default", "Prints a message to the output"
+
+attribute "vaidya_chef/custom_input",
+  :display_name => "A Custom Chef Input",
+  :choice => ["A", "B", "C"],
+  :recipes => ["vaidya_chef::default"]
+
--- cookbooks/vaidya_chef/recipes/default.rb
+++ cookbooks/vaidya_chef/recipes/default.rb
@@ -11,4 +11,17 @@ rs_utils_marker :begin
 
 log "HELLO"
 
+package_name = value_for_platform(
+ ["centos", "redhat"] => {"default" => "httpd"}
+ ["ubuntu", "debian"] => { "default" => "apache2" }
+  )
+
+package package_name do
+action :install
+end
+
+log "The package that will be installed is #{package_name}
+
+log "The input from the dashboard is #{node[:vaidya_chef][:custom_input]}"
+
 rs_utils_marker :end
